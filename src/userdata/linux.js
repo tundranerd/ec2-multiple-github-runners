c7727f9c94ef3c608ac4cc3c9da20da62ae4ea0e
John Haynes <tundranerd@gmail.com>
2022-04-09 13:07:41 -0600

    Fix parameters for GH_TOKEN

--- src/userdata/linux.js
+++ src/userdata/linux.js
@@ -4,16 +4,16 @@ const core = require('@actions/core');
 const globalConfig = [
       '#!/bin/bash',
       'apt-get update && apt-get install libicu60 -y',
-      'mkdir /tmp && cd /tmp',
+      'mkdir -p /tmp && cd /tmp',
       'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
       'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
 ].join("\n");
 
-function createRegistration(label, i, githubRegistrationToken) {
+function createRegistration(label, githubRegistrationToken) {
   return [
-      `mkdir /tmp/runner-${i} && cd /tmp/runner-${i}`,
-      `tar xzf /tmp/actions-runner-linux-\${RUNNER_ARCH}-2.286.0.tar.gz -C /tmp/runner-${i}`,
+      'mkdir -p /tmp/runner && cd /tmp/runner',
+      `tar xzf /tmp/actions-runner-linux-\${RUNNER_ARCH}-2.286.0.tar.gz -C /tmp/runner`,
+      'export RUNNER_ALLOW_RUNASROOT=1',
       `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --name ${label} --unattended`,
       './run.sh 2>&1 &'
   ].join("\n");
