35b4c91ab15cb0d39c7d5819bee46b32f93ec169
John Haynes <tundranerd@gmail.com>
2024-02-29 14:33:45 -0700

    Use latest windows x64 runner

--- src/userdata/windows.js
+++ src/userdata/windows.js
@@ -8,17 +8,17 @@ const globalConfig = [
   // Create runner dir
   'mkdir C:\\actions-runner; cd C:\\actions-runner',
   // Download GitHub Runner
-  'Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-win-x64-2.311.0.zip -OutFile actions-runner-win-x64-2.311.0.zip',
+  'Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/v2.313.0/actions-runner-win-x64-2.313.0.zip -OutFile actions-runner-win-x64-2.313.0.zip',
   // Check hash is good
-  'if((Get-FileHash -Path actions-runner-win-x64-2.311.0.zip -Algorithm SHA256).Hash.ToUpper() -ne \'e629628ce25c1a7032d845f12dfe3dced630ca13a878b037dde77f5683b039dd\'.ToUpper()){ throw \'Computed checksum did not match\' }',
+  'if((Get-FileHash -Path actions-runner-win-x64-2.313.0.zip -Algorithm SHA256).Hash.ToUpper() -ne \'c4cb3e5d9f0ab42ddc224cfdf9fb705397a7b20fd321536da5500259225fdf8a\'.ToUpper()){ throw \'Computed checksum did not match\' }',
 ].join("\n");
 
 function createRegistration(label, githubRegistrationToken) {
   return [
     // Extract runner .zip
-    'Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.311.0.zip", "$PWD")',
+    'Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD/actions-runner-win-x64-2.313.0.zip", "$PWD")',
     // Configure the runner for the current repo
-    `.\\config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --name ${label} --unattended  --disableupdate`,
+    `.\\config.cmd --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label} --name ${label} --unattended`,
     // Run it!
     'start-process -Filepath run.cmd'
   ].join("\n");
